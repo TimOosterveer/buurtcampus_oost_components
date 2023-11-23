a6da8be640b46f2a3e8ccf9c16e0f4eefe736302
TimOosterveer <62908209+TimOosterveer@users.noreply.github.com>
2023-11-23 11:41:42 +0100

    Update Card

--- src/lib/molecules/CardWorkshop.svelte
+++ src/lib/molecules/CardWorkshop.svelte
@@ -5,57 +5,34 @@
 
 {#each data.workshops as workshop}
 	<section>
-    <img src={workshop.foto[0].url} alt="">
+		<img src={workshop.foto[0].url} alt="" />
 
-   
-
-        <h2>{workshop.naam}</h2>
-  </section>
+		<h2>{workshop.naam}</h2>
+	</section>
 {/each}
 
-
-
 <style>
-
-  section {
-    position: relative;
-    border-radius: var(--bradius);
-    height: 40rem;
-    padding: var(--padding);
-  }
+	section {
+		position: relative;
+		border-radius: var(--bradius);
+		height: 40rem;
+    margin: var(--margin);
+	}
 
   
-  /* section::after {
-    content: '';
-		position: absolute;
-		background-color: #2b2b2b;
+	img {
+    position: absolute;
     border-radius: var(--bradius);
-		width: 100%;
-		height: 100%;
-		top: 0;
-		left: 0;
-		opacity: .4;
-		z-index: 0;
-  } */
-  
-  img {
-    width: 100%;
+    top: 0;
+    left: 0;
     height: 100%;
+    width: 100%;
     object-fit: cover;
-    border-radius: var(--bradius);
+    z-index: -1;
+  }
+  
+  h2 {
+    color: white;
   }
 
-  section::after {
-    content: '';
-		position: absolute;
-		background-color: #2b2b2b;
-    border-radius: var(--bradius);
-		width: 100%;
-		height: 100%;
-		top: 0;
-		left: 0;
-		opacity: .4;
-		z-index: 0;
-    margin: 2rem;
-  }  
 </style>
