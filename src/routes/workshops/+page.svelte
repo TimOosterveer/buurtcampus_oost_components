a7b9acb9c8a28dc0bf5358c40028236ea2498078
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-11-28 09:17:09 +0100

    headere and footer

--- src/routes/workshops/+page.svelte
+++ src/routes/workshops/+page.svelte
@@ -2,7 +2,14 @@
   export let data;
   import Info from '$lib/organisms/InfoWorkshop.svelte'
   import Card from '$lib/organisms/OverviewWorkshops.svelte'
+  import Footer from '../../lib/organisms/Footer.svelte';
+  import NavDark from '../../lib/organisms/NavDark.svelte';
+  import MobileMenu from '../../lib/organisms/MobileMenu.svelte';
 </script>
 
+<NavDark />
+<MobileMenu />
 <Info/>
 <Card data={data}/>
+
+<Footer />
