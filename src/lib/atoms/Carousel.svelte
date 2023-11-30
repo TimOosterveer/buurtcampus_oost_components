5334da034771ceec80a2abea87e8cab20823c19e
joelle78 <96306220+joelle78@users.noreply.github.com>
2023-11-30 13:03:43 +0100

    aria labels

--- src/lib/atoms/Carousel.svelte
+++ src/lib/atoms/Carousel.svelte
@@ -1,161 +1,164 @@
 <script>
-	import { onDestroy, onMount } from 'svelte';
-	import image1 from '$lib/assets/carousel-img-1.jpg';
-	import image2 from '$lib/assets/carousel-img-2.jpg';
-	import image3 from '$lib/assets/carousel-img-3.jpg';
-	import ButtonGelijkSwappen from './ButtonGelijkSwappen.svelte';
+    import {onDestroy, onMount} from 'svelte';
+    import image1 from '$lib/assets/carousel-img-1.jpg';
+    import image2 from '$lib/assets/carousel-img-2.jpg';
+    import image3 from '$lib/assets/carousel-img-3.jpg';
+    import ButtonGelijkSwappen from './ButtonGelijkSwappen.svelte';
 
-	let intervalId;
+    let intervalId;
 
-	onMount(() => {
-		const carrouselContainer = document.querySelector('.carrousel');
-		const carrousel = document.querySelector('.carrousel-a');
+    onMount(() => {
+        const carrouselContainer = document.querySelector('.carrousel');
+        const carrousel = document.querySelector('.carrousel-a');
 
-		const interval = 8000;
+        const interval = 8000;
 
-		intervalId = setInterval(() => {
-			const currentScroll = carrousel.scrollLeft;
-			const itemWidth = carrouselContainer.clientWidth;
-			const totalWidth = carrousel.scrollWidth;
+        intervalId = setInterval(() => {
+            const currentScroll = carrousel.scrollLeft;
+            const itemWidth = carrouselContainer.clientWidth;
+            const totalWidth = carrousel.scrollWidth;
 
-			const nextScroll = (currentScroll + itemWidth) % totalWidth;
+            const nextScroll = (currentScroll + itemWidth) % totalWidth;
 
-			carrousel.scrollTo({
-				left: nextScroll,
-				behavior: 'smooth'
-			});
-		}, interval);
-	});
+            carrousel.scrollTo({
+                left: nextScroll,
+                behavior: 'smooth'
+            });
+        }, interval);
+    });
 
-	onDestroy(() => {
-		clearInterval(intervalId);
-	});
+    onDestroy(() => {
+        clearInterval(intervalId);
+    });
 </script>
 
 <main>
-	<div class="carrousel" aria-label="Carousel">
-		<div class="overlay"></div>
+    <div class="carrousel" aria-label="Carousel">
+        <div class="overlay"></div>
 
-		<h1>Swap nu je stekjes!</h1>
+        <h1>Swap nu je stekjes!</h1>
 
-		<ButtonGelijkSwappen />
-		<div class="carrousel-a">
-			<div class="carrousel-picture">
-				<img src={image1} alt="Slide 1" aria-label="carousel-image-1" />
-			</div>
-			<div class="carrousel-picture">
-				<img src={image2} loading="”lazy”" alt="Slide 2" aria-label="carousel-image-2" />
-			</div>
-			<div class="carrousel-picture">
-				<img src={image3} loading="”lazy”" alt="Slide 3" aria-label="carousel-image-3" />
-			</div>
-		</div>
-	</div>
+        <ButtonGelijkSwappen/>
+        <div class="carrousel-a" role="list" tabindex="0" aria-live="polite">
+            <div class="carrousel-picture">
+                <img src={image1} alt="Slide 1" aria-label="foto-van-stekjes"/>
+            </div>
+            <div class="carrousel-picture">
+                <img src={image2} loading="”lazy”" alt="Slide 2" aria-label="foto-van-stekjes"/>
+            </div>
+            <div class="carrousel-picture">
+                <img src={image3} loading="”lazy”" alt="Slide 3" aria-label="foto-van-een-stekje-die-word-gepot"/>
+            </div>
+        </div>
+    </div>
 </main>
 
 <style>
-	main {
-		background-color: var(--background-color);
-	}
+    main {
+        background-color: var(--background-color);
+    }
 
-	.carrousel {
-		display: flex;
-			justify-content: center;
-			align-items: center;
-		width: 100%;
-		height: 100vh;
-	}
+    .carrousel {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100vh;
 
-	.overlay {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.1) 100%);
-		z-index: 1;
-	}
+    }
 
-	.carrousel h1 {
-		position: absolute;
-		z-index: 1;
-		width: 60%;
-		color: var(--text-color);
-		display: flex;
-		text-align: center;
-		justify-content: center;
-		align-items: center;
+    .overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.1) 100%);
+        z-index: 1;
+    }
+
+    .carrousel h1 {
+        position: absolute;
+        z-index: 1;
+        width: 60%;
+        color: var(--text-color);
+        display: flex;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
         font-weight: 900;
-		line-height: 1.1em;
-		font-size: rem;
-	}
+        line-height: 1.1em;
+        /*font-size: rem;*/
+    }
 
-	.carrousel img {
-		width: 100%;
-		height: 100vh;
-		/* aspect-ratio: 16 / 9; */
-		object-fit: cover;
-	}
+    .carrousel img {
+        width: 100%;
+        height: 100vh;
+        object-fit: cover;
 
-	.carrousel-a {
-		display: flex;
-		overflow: auto;
-		scroll-snap-type: x mandatory;
-		scrollbar-width: none;
-	}
+    }
 
-	.carrousel-a::-webkit-scrollbar {
-		display: none;
-	}
+    .carrousel-a {
+        display: flex;
+        overflow-x: auto;
+        scroll-snap-type: x mandatory;
+        scrollbar-width: none;
 
-	.carrousel-picture {
-		flex-grow: 0;
-		flex-shrink: 0;
-		flex-basis: 100%;
-		scroll-snap-align: center;
-	}
+    }
 
-	/* MEDIA QUERY TABLET*/
-	@media screen and (min-width: 48rem) {
-		.overlay {
-			height: 42.2%;
-			background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.1) 100%);
-		}
+    .carrousel-a::-webkit-scrollbar {
+        display: none;
 
-		.carrousel h1 {
-			width: 50%;
-			height: 45%;
-			font-size: 3rem;
-		}
-	}
+    }
 
-	/* MEDIA QUERY DESKTOP */
-	@media screen and (min-width: 64rem) {
-		main {
+    .carrousel-picture {
+        flex-grow: 0;
+        flex-shrink: 0;
+        flex-basis: 100%;
+        scroll-snap-align: start;
+    }
 
-		}
+    /* MEDIA QUERY TABLET*/
+    @media screen and (min-width: 48rem) {
+        .overlay {
+            height: 42.2%;
+            background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.1) 100%);
+        }
 
-		.carrousel {
-			height: 100vh;
-			width: 100%;
-		}
+        .carrousel h1 {
+            width: 50%;
+            height: 45%;
+            font-size: 3rem;
+        }
+    }
 
-		.overlay {
-			height: 100%;
-			background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.1) 100%);
-		}
+    /* MEDIA QUERY DESKTOP */
+    @media screen and (min-width: 64rem) {
+        main {
 
-		.carrousel {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-		}
-        
-		.carrousel h1 {
-			width: 60%;
-			height: 100%;
-			font-size: 8vw;
-			line-height: 7rem;
-		}
-	}
+        }
+
+        .carrousel {
+            height: 100vh;
+            width: 100%;
+        }
+
+        .overlay {
+            height: 100%;
+            background: linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.1) 100%);
+        }
+
+        .carrousel {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .carrousel h1 {
+            width: 60%;
+            height: 100%;
+            font-size: 8vw;
+            line-height: 7rem;
+        }
+    }
 </style>
